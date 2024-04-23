1de52a1ee061b87cecf5e07ed21e4b59a1925d97
rany <ranygh@riseup.net>
2024-04-23 12:53:40 +0300

    Show % of users that migrated in maint/get_fp_stats.sh
    Signed-off-by: rany <ranygh@riseup.net>

--- maint/get_fp_stats.sh
+++ maint/get_fp_stats.sh
@@ -9,7 +9,7 @@
 set -euf -o pipefail
 export LC_ALL=en_US.UTF-8  # for printf to use comma as thousands separator
 
-if [ "$#" -lt 1 ]; then
+if [[ "$#" -lt 1 ]]; then
     echo >&2 "Usage: $0 <date>"
     echo >&2
     echo >&2 "  <date> could be a date string like 'yesterday' or '2 days ago'"
@@ -22,11 +22,23 @@ fi
 #date=$(date -u +%Y/%m/%d -d "2 days ago")
 date=$(date -u '+%Y/%m/%d' -d "$*")
 
-for app_id in com.github.Eloston.UngoogledChromium io.github.ungoogled_software.ungoogled_chromium; do
-    printf "%s: " "$app_id"
-    curl --silent --compressed --fail "https://hub.flathub.org/stats/stable/${date:?}.json" | \
-        jq --arg app_id "$app_id" '.refs[$app_id] // [] | map(.[] | select(. != null))' | \
-        awk 'BEGIN {sum=0} {sum+=$1} END {printf "%'"'"'d\n", sum}'
+declare -A app_ids
+old_app_id=com.github.Eloston.UngoogledChromium
+new_app_id=io.github.ungoogled_software.ungoogled_chromium
+declare -i old_sum new_sum
+
+for app_id in "${old_app_id}" "${new_app_id}"; do
+    printf "%s: " "${app_id}"
+    sum=$(curl --silent --compressed --fail "https://hub.flathub.org/stats/stable/${date:?}.json" | \
+          jq --arg app_id "${app_id}" '.refs[$app_id] // [] | map(.[] | select(. != null))' | \
+          awk 'BEGIN {sum=0} {sum+=$1} END {print sum}')
+    printf "%'d\n" "${sum}"
+    app_ids["${app_id}"]=${sum}
 done
+
+old_sum=${app_ids[${old_app_id}]}
+new_sum=${app_ids[${new_app_id}]}
+percentage=$(awk "BEGIN {print ${new_sum} / (${old_sum} + ${new_sum}) * 100}")
+printf "Percentage of users that have migrated: %.2f%%\n" "${percentage}"
 
 exit 0
